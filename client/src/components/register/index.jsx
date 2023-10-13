32212dae04ef996470521ea42f3cc16d4746093d
Velki Daza <vdaza@ulearning.com.co>
2023-10-13 11:50:25 -0500

    register esta listo

--- client/src/components/register/index.jsx
+++ client/src/components/register/index.jsx
@@ -1,11 +1,61 @@
-import React from "react";
-import { Fondo } from "./styled";
+import React, { useState } from 'react';
+import { Fondo,Form,Label,Input,Button } from './styled';
 
 const Register = () => {
-    return(
-        <Fondo>
-            <h1> hola </h1>
-        </Fondo>
-    )
-}
-export default Register
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes realizar acciones adicionales, como enviar los datos a un servidor.
+    console.log('Form data:', formData);
+  };
+
+  return (
+    <Fondo>
+    <Form onSubmit={handleSubmit}>
+      <div>
+        <Label>Username:</Label>
+        <Input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <Label>Email:</Label>
+        <Input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <Label>Password:</Label>
+        <Input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </div><br />
+      <Button type="submit">Register</Button>
+    </Form>
+    </Fondo>
+  );
+};
+
+export default Register;
--- client/src/components/register/styled.jsx
+++ client/src/components/register/styled.jsx
@@ -1,7 +1,48 @@
 import styled from "styled-components";
 
 export const  Fondo = styled.body`
-    background: red;
+    /* background: red; */
     width: 100%;
     height: 97vh;
+    display:flex;
+    justify-content:center;
+    align-items: center;
+`
+export const Form = styled.form`
+    background-color: #e2a0a0;
+    padding: 40px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+    max-width: 300px;
+    margin: auto;
+
+`
+export const Label = styled.label`
+    display: block;
+   margin-bottom: 5px;
+   color: #333;
+`
+export const Input = styled.input`
+    width: 100%;
+    padding: 10px;
+    font-size: 16px;
+    border-radius: 5px;
+    border: 1px solid #ddd;
+`
+export const Button = styled.button`
+    width: 90%;
+    padding: 15px;
+    border-radius: 5px;
+    background:#0e5cb0 ;
+    color: white;
+    border: none;
+    font-size: 16px;
+    text-transform: uppercase;
+    cursor: pointer;
+    :hover{
+    background-color: #45a049;
+  }
+    
 `
