78a690fcf3483d3a5174e4bf6aec3c0682728c0d
Velki Daza <vdaza@ulearning.com.co>
2023-10-13 10:28:21 -0500

    organizacion del frontend

---
+++ client/src/components/register/styled.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const  Fondo = styled.body`
+    background: red;
+    width: 100%;
+    height: 97vh;
+`
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Register from './components/register/index ';
+import Register from './components/register';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
