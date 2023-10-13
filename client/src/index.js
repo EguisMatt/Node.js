6cf89e2dfa28076ef582c525aa472c333c190e88
FundacionET <fundacionet4@gmail.com>
2023-10-13 17:17:02 -0500

    cambios

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import Login from "./components/Login";
-//import Register from './components/register';
+import Register from './components/register';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Login/>
-    {/* <Register/> */}
+    <Register/>
   </React.StrictMode>
 );
 
