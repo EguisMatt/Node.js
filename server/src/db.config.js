dff29d4a193bd135c24035f59f5e17abce4c9207
Velki Daza <vdaza@ulearning.com.co>
2023-10-13 13:14:31 -0500

    ip local

--- server/src/db.config.js
+++ server/src/db.config.js
@@ -1,6 +1,7 @@
 import createpool from "mysql2/promise";
+
 export const pool = createpool({
-    host: 'localhost',
+    host: ' 192.168.12.36',
     user: 'root',
     passworod: "",
     port: 3306,
